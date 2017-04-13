0461f11d4cf4e21d5c8e467dcc2707ee66f64a92
Dom Clemmer <dclemmer@Dom-Clemmer.local>
2017-04-13 10:54:15 -0400

    streamline workflow with gulp

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const gutil = require('gulp-util');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+gulp.task('sass', function(){
+  return gulp.src('./public/stylesheets/home.sass')
+        .pipe(sass({style: 'compressed'}))
+        .pipe(postcss([ autoprefixer() ]))
+        .on('error', gutil.log)
+        .pipe(gulp.dest('./public/stylesheets'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('public/*.sass', ['sass']);
+});
+
+gulp.task('default', ['watch', 'sass']);
