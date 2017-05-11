b19331cec8a3a3cb548b5152efacd0d40b0f766c
Dom Clemmer <thisisdom4@gmail.com>
2017-05-11 11:36:40 -0400

    add test logic for recieving a new message

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ var moment = require('moment');
 var app = express();
 
 // MIDDLEWARE
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
@@ -24,29 +22,59 @@ app.set('view engine', 'jade');
 // LANDING PAGE & MAIN APP
 app.get('/', function(req, res) {
 
-    res.render('homepage', ({
-        date: moment().format("ddd, MMM Do YY"),
-        quote: serveAffirmation()
-    }));
+    res.render('homepage', ({date: moment().format("ddd, MMM Do YY"), quote: serveAffirmation()}));
 });
 
 // FACEBOOK MESSENGER BOT
 app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === 'jai_jai_ganesha') {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
-  }
+    var accessToken = 'EAAFTJz88HJUBAJqx5WkPGiIi0jPRyBXmpuN56vZB0FowKCZCzej8zpM4hKTt2ZCXqDZASqL4GUC5ywuOjakob1icM4Sfa4L3xcpsTKsjHl0QHzPylbHjJakyq1hcPNA4i8wt7XjsGZBGoUNYP7Yx2hg8RYiG9xzUoo0dzuThqGwZDZD'
+    if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'jai_jai_ganesha') {
+        console.log("Validating webhook");
+        res.status(200).send(req.query['hub.challenge'])
+    } else {
+        console.error("Failed validation. Make sure the validation tokens match.")
+        res.sendStatus(403)
+    }
+})
+
+app.post('/webhook', function(req, res) {
+    var data = req.body
+
+    // Make sure this is a page subscription
+    if (data.object === 'page') {
+
+        // Iterate over each entry - there may be multiple if batched
+        data.entry.forEach(function(entry) {
+            var pageID = entry.id
+            var timeOfEvent = entry.time
+
+            // Iterate over each messaging event
+            entry.messaging.forEach(function(event) {
+                if (event.message) {
+                    receivedMessage(event)
+                } else {
+                    console.log("Webhook received unknown event: ", event)
+                }
+            });
+        });
+
+        // Assume all went well.
+        //
+        // You must send back a 200, within 20 seconds, to let us know
+        // you've successfully received the callback. Otherwise, the request
+        // will time out and we will keep trying to resend.
+        res.sendStatus(200)
+    }
 });
+
+function receivedMessage(event) {
+    // Putting a stub for now, we'll expand it in the following steps
+    console.log("Message data: ", event.message);
+}
 
 // ADMIN APP
 app.get('/admin', function(req, res) {
-    res.render('admin', ({
-        affirmations: db.affirmations.find()
-    }));
+    res.render('admin', ({affirmations: db.affirmations.find()}));
 });
 
 app.post('/new_affirmation', function(req, res) {
@@ -58,12 +86,9 @@ app.post('/new_affirmation', function(req, res) {
     res.redirect('admin');
 });
 
-
 // HELPER FUNCTIONS
 function serveAffirmation() {
-    var unused = db.affirmations.find({
-        used: false
-    })
+    var unused = db.affirmations.find({used: false})
     return unused[Math.floor(Math.random() * unused.length)].affirmation_text
 }
 
