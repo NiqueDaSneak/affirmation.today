17d40030d0cd125b6efce0bc405b4444db8eca63
Dom Clemmer <thisisdom4@gmail.com>
2017-05-11 11:55:32 -0400

    testing fix error

--- server.js
+++ server.js
@@ -70,99 +70,99 @@ function receivedMessage(event) {
 }
 
 function receivedMessage(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
-  var message = event.message;
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfMessage = event.timestamp;
+    var message = event.message;
 
-  console.log("Received message for user %d and page %d at %d with message:",
-    senderID, recipientID, timeOfMessage);
-  console.log(JSON.stringify(message));
+    console.log("Received message for user %d and page %d at %d with message:", senderID, recipientID, timeOfMessage);
+    console.log(JSON.stringify(message));
 
-  var messageId = message.mid;
+    var messageId = message.mid;
 
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
+    var messageText = message.text;
+    var messageAttachments = message.attachments;
 
-  if (messageText) {
+    if (messageText) {
 
-    // If we receive a text message, check to see if it matches a keyword
-    // and send back the example. Otherwise, just echo the text we received.
-    switch (messageText) {
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
+        // If we receive a text message, check to see if it matches a keyword
+        // and send back the example. Otherwise, just echo the text we received.
+        switch (messageText) {
+            case 'generic':
+                sendGenericMessage(senderID);
+                break;
 
-      default:
-        sendTextMessage(senderID, messageText);
+            default:
+                sendTextMessage(senderID, messageText);
+        }
+    } else if (messageAttachments) {
+        sendTextMessage(senderID, "Message with attachment received");
     }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
-  }
 }
 
 function sendGenericMessage(recipientId, messageText) {
-  // To be expanded in later sections
+    // To be expanded in later sections
 }
 
 function sendTextMessage(recipientId, messageText) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: messageText
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            text: messageText
+        }
+    };
+
+    function callSendAPI(messageData) {
+        request({
+            uri: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: {
+                access_token: PAGE_ACCESS_TOKEN
+            },
+            method: 'POST',
+            json: messageData
+
+        }, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var recipientId = body.recipient_id;
+                var messageId = body.message_id;
+
+                console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
+            } else {
+                console.error("Unable to send message.");
+                console.error(response);
+                console.error(error);
+            }
+        });
     }
-  };
 
-  function callSendAPI(messageData) {
-  request({
-    uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: PAGE_ACCESS_TOKEN },
-    method: 'POST',
-    json: messageData
+    // ADMIN APP
+    app.get('/admin', function(req, res) {
+        res.render('admin', ({affirmations: db.affirmations.find()}));
+    });
 
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
+    app.post('/new_affirmation', function(req, res) {
+        var new_affirmation = {
+            used: false,
+            affirmation_text: req.body.affirmation_text
+        }
+        db.affirmations.save(new_affirmation);
+        res.redirect('admin');
+    });
 
-      console.log("Successfully sent generic message with id %s to recipient %s",
-        messageId, recipientId);
-    } else {
-      console.error("Unable to send message.");
-      console.error(response);
-      console.error(error);
+    // HELPER FUNCTIONS
+    function serveAffirmation() {
+        var unused = db.affirmations.find({used: false})
+        return unused[Math.floor(Math.random() * unused.length)].affirmation_text
     }
-  });
-}
 
-// ADMIN APP
-app.get('/admin', function(req, res) {
-    res.render('admin', ({affirmations: db.affirmations.find()}));
-});
-
-app.post('/new_affirmation', function(req, res) {
-    var new_affirmation = {
-        used: false,
-        affirmation_text: req.body.affirmation_text
-    }
-    db.affirmations.save(new_affirmation);
-    res.redirect('admin');
-});
-
-// HELPER FUNCTIONS
-function serveAffirmation() {
-    var unused = db.affirmations.find({used: false})
-    return unused[Math.floor(Math.random() * unused.length)].affirmation_text
-}
-
-// SERVER LISTENING
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-    console.log('Server running on port ' + port);
-});
-app.on('error', function() {
-    console.log(error);
-});
-module.exports = app;
+    // SERVER LISTENING
+    var port = process.env.PORT || 3000;
+    app.listen(port, function() {
+        console.log('Server running on port ' + port);
+    });
+    app.on('error', function() {
+        console.log(error);
+    });
+    module.exports = app;
