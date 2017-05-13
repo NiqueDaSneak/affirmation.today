ef686641a5f0a2553fdd19ccd5142764e9801c46
Dom Clemmer <thisisdom4@gmail.com>
2017-05-13 16:54:24 -0400

    add missing brace

--- server.js
+++ server.js
@@ -157,8 +157,7 @@ function sendGenericTemplateButtons(recipientId, messageText) {
                             "type": "postback",
                             "title": "Yes I would",
                             "payload": "YES_SCHEDULE_MSG"
-                        },
-                        {
+                        }, {
                             "type": "postback",
                             "title": "Not Interested",
                             "payload": "NO_SCHEDULE_MSG"
@@ -166,53 +165,54 @@ function sendGenericTemplateButtons(recipientId, messageText) {
                     ]
                 }
             }
-            callSendAPI(messageData);
         }
+        callSendAPI(messageData);
+    }
 
-        function sendTextMessage(recipientId, messageText) {
-            var messageData = {
-                recipient: {
-                    id: recipientId
-                },
-                message: {
-                    text: messageText
-                }
-            };
+    function sendTextMessage(recipientId, messageText) {
+        var messageData = {
+            recipient: {
+                id: recipientId
+            },
+            message: {
+                text: messageText
+            }
+        };
 
-            callSendAPI(messageData);
-        }
+        callSendAPI(messageData);
+    }
 
-        function callSendAPI(messageData) {
-            request({
-                uri: 'https://graph.facebook.com/v2.6/me/messages',
-                qs: {
-                    access_token: PAGE_ACCESS_TOKEN
-                },
-                method: 'POST',
-                json: messageData
+    function callSendAPI(messageData) {
+        request({
+            uri: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: {
+                access_token: PAGE_ACCESS_TOKEN
+            },
+            method: 'POST',
+            json: messageData
 
-            }, function(error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    var recipientId = body.recipient_id;
-                    var messageId = body.message_id;
+        }, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var recipientId = body.recipient_id;
+                var messageId = body.message_id;
 
-                    console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
-                } else {
-                    console.error("Unable to send message.");
-                    console.error(response);
-                    console.error(error);
-                }
-            });
-        }
-
-        // HELPER FUNCTIONS
-
-        // SERVER LISTENING
-        var port = process.env.PORT || 3000;
-        app.listen(port, function() {
-            console.log('Server running on port ' + port);
+                console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
+            } else {
+                console.error("Unable to send message.");
+                console.error(response);
+                console.error(error);
+            }
         });
-        app.on('error', function() {
-            console.log(error);
-        });
-        module.exports = app;
+    }
+
+    // HELPER FUNCTIONS
+
+    // SERVER LISTENING
+    var port = process.env.PORT || 3000;
+    app.listen(port, function() {
+        console.log('Server running on port ' + port);
+    });
+    app.on('error', function() {
+        console.log(error);
+    });
+    module.exports = app;
