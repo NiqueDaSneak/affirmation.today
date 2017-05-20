4c28f6f51421c3e2b7fec01db9b06778344e9ca1
Dom Clemmer <thisisdom4@gmail.com>
2017-05-20 17:18:58 -0400

    test scheduler

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.post('/webhook', function(req, res) {
 
 // SCHEDULER
 var scheduler = require('node-schedule')
-var job = scheduler.scheduleJob('44 19 * * * *', function(){
+var job = scheduler.scheduleJob('44 21 * * * *', function(){
   User.find({ timeOfDay: 'morning' }, (err, users) => {
     if (err) return console.log(err)
       for (var i = 0; i < users.length; i++) {
