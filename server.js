7b11e6142bf4221400d31a0d4344761dbe85b940
Dom Clemmer <thisisdom4@gmail.com>
2017-06-05 13:15:16 -0400

    implement user auth for getStarted postback

--- server.js
+++ server.js
@@ -115,26 +115,24 @@ function eventHandler(event) {
                    console.log(user)
                    existingUser = user
                 })
-                console.log('existingUser: ' + existingUser)
                 if (existingUser) {
-                  console.log('This user exists')
+                  sendTextMessage(senderID, 'Welcome back! Use the menu for your actions!')
                 } else {
-                  console.log('this user does not exist')
+                  request({
+                      uri: 'https://graph.facebook.com/v2.6/' + senderID + '?access_token=EAAFTJz88HJUBAJqx5WkPGiIi0jPRyBXmpuN56vZB0FowKCZCzej8zpM4hKTt2ZCXqDZASqL4GUC5ywuOjakob1icM4Sfa4L3xcpsTKsjHl0QHzPylbHjJakyq1hcPNA4i8wt7XjsGZBGoUNYP7Yx2hg8RYiG9xzUoo0dzuThqGwZDZD',
+                      method: 'GET'
+                  }, function(error, response, body) {
+                      if (error) {
+                          return console.error('upload failed:', error);
+                      }
+                      var data = JSON.parse(body)
+                      var newUser = new User({fbID: senderID, fullName: data.first_name + ' ' + data.last_name, photo: data.profile_pic, enrolled: false, timezone: data.timezone})
+                      newUser.save((err, user) => {
+                        if (err) return console.error(err)
+                      })
+                      sendWelcomeMessage(senderID, 'Hello '+ data.first_name +'! Welcome to Affirmation.today! Would you like to sign up for reoccuring messages')
+                  })
                 }
-                // request({
-                //     uri: 'https://graph.facebook.com/v2.6/' + senderID + '?access_token=EAAFTJz88HJUBAJqx5WkPGiIi0jPRyBXmpuN56vZB0FowKCZCzej8zpM4hKTt2ZCXqDZASqL4GUC5ywuOjakob1icM4Sfa4L3xcpsTKsjHl0QHzPylbHjJakyq1hcPNA4i8wt7XjsGZBGoUNYP7Yx2hg8RYiG9xzUoo0dzuThqGwZDZD',
-                //     method: 'GET'
-                // }, function(error, response, body) {
-                //     if (error) {
-                //         return console.error('upload failed:', error);
-                //     }
-                //     var data = JSON.parse(body)
-                //     var newUser = new User({fbID: senderID, fullName: data.first_name + ' ' + data.last_name, photo: data.profile_pic, enrolled: false, timezone: data.timezone})
-                //     newUser.save((err, user) => {
-                //       if (err) return console.error(err)
-                //     })
-                //     sendWelcomeMessage(senderID, 'Hello '+ data.first_name +'! Welcome to Affirmation.today! Would you like to sign up for reoccuring messages')
-                // })
                 break
             case 'YES_SCHEDULE_MSG':
                 sendTextMessage(senderID, "You've been enrolled! Look for your affirmations to start coming tomorrow!")
