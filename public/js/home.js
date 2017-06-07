376b0e5ac9e620ab6e6c39bdc62e3f19de3b13a7
Dom Clemmer <thisisdom4@gmail.com>
2017-06-06 21:02:25 -0400

    change order of animations

--- public/js/home.js
+++ public/js/home.js
@@ -6,25 +6,25 @@ $(document).ready(function() {
   var rand = Math.floor((Math.random() * 19) + 1);
   if (/Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     $('body').css('background-image', "url('/img/mobile/image" + rand + ".jpg')")
-    $('.messenger').css('bottom', '20vh')
     $('.subscription span').css('bottom', '0vh')
+    $('.messenger').css('bottom', '20vh')
     setTimeout(() => {
       $('.subscription span').css('bottom', '-16vh')
     }, 5000)
   } else if (/iPad/i.test(navigator.userAgent)) {
     $('body').css('background-image', "url('/img/tablet/image" + rand + ".jpg')")
-    $('.messenger').css('bottom', '20vh')
     $('.subscription span').css('bottom', '0vh')
+    $('.messenger').css('bottom', '20vh')
     setTimeout(() => {
       $('.subscription span').css('bottom', '-16vh')
     }, 5000)
   } else {
     $('body').css('background-image', "url('/img/desktop/image" + rand + ".jpg')")
-    $('.messenger').css('bottom', '14vh')
     $('.subscription span').css('bottom', '0vh')
+    $('.messenger').css('bottom', '14vh')
     setTimeout(() => {
       $('.subscription span').css('bottom', '-16vh')
     }, 5000)
   }
-  
+
 })
